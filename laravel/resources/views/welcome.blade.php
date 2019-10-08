e9a0b1864616c76ef9ed2eb45b13dc88ff336db2
vietawake <vietawake@gmail.com>
2019-10-08 23:51:10 +0700

    asxsaxdd

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -87,7 +87,7 @@
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs sdd Viet</a>
                     <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://laravel-news.com">Newsdasds</a>
                     <a href="https://blog.laravel.com">Blog</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>

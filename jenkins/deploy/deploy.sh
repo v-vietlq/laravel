4b9093136804500966fafdb85979c0f5e5cf6ba6
vietawake <vietawake@gmail.com>
2019-10-08 15:07:24 +0700

    asdsa

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -2,5 +2,5 @@
 
 
 scp -i /opt/prod ./jenkins/deploy/publish root@144.202.3.128:/tmp/publish
-scp -i /opt/prod -r laravel/* root@144.202.3.128:~/laravel/
+scp -i /opt/prod -prq laravel/ root@144.202.3.128:~/
 ssh -i /opt/prod root@144.202.3.128 "sudo bash /tmp/publish"

1a94da201099f1d59cfefcfbc7e395dc29fe3e30
vietawake <vietawake@gmail.com>
2019-10-11 13:26:26 +0700

    abc

---
+++ laravel/app/Http/Controllers/Viet.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Viet extends Controller
+{
+    public function hello() {
+        return 'Hello World viet';
+    }
+}
--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -78,10 +78,9 @@
                     @endauth
                 </div>
             @endif
-
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel le
+                    Laravel OK
                 </div>
 
                 <div class="links">
